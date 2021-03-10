ba39930e9ba3dc2b85ca032b22b9b7d3a60831b3
LuisPeMoraRod <luismorarod98@gmail.com>
2021-03-09 20:13:24 -0600

    Add begin button

--- gui.rkt
+++ gui.rkt
@@ -5,6 +5,7 @@
          )
 
 (define *players* null)
+(define *x_croupier* 360)
 
 ;************************* Welcome Window *************************
 #|Define frame for the welcome window, where the name(s) of the player(s) are requested|#
@@ -68,7 +69,16 @@
        [width 800]
        [height 540]
        [style (list 'no-resize-border)]
+       [alignment (list 'left 'center)]
        ))
+
+#|Define label that shows the player who has the current turn|#
+(define turn-mssg 
+(new message%
+       [parent game-window]
+       [label "Press 'Begin' button to start the game..."]
+       [vert-margin 2]
+       [horiz-margin 10]))
 
 #|Deletes null lists from a list
 @param lst : list with sublists that contain the names received from text inputs (may contain empty sublists)
@@ -76,15 +86,7 @@
 (define (create-players-list lst)
   (cond ((null? lst) '() )
         ((equal? (car lst) "") (create-players-list (cdr lst)))
-        (else (cons (list (car lst)) (create-players-list (cdr lst))))))
-
-#|Define label that shows the player who has the current turn|#
-(define turn-mssg 
-(new message%
-       [parent game-window]
-       [label "It's your turn: "]
-       [vert-margin 2]
-       [horiz-margin 10]))
+        (else (cons (list (car lst) '() #t) (create-players-list (cdr lst))))))
 
 #|Get the name of the player with the current turn
 @param players : list with the names and list of cards of the players
@@ -101,28 +103,105 @@
 #|Sets the name of the player who has the current turn in the turn-mssg label
 @param name : string|#
 (define (set-turn-mssg name) 
-  {send turn-mssg set-label (string-join (list (send turn-mssg get-label) name))})
+  {send turn-mssg set-label (string-join (list "It's your turn: " name))})
 
 #|Creates the main canvas where the cards will be displayed
 @param frame : frame%
 @param table-bitmap : bitmap%|#
-(define (main-canvas frame table-bitmap)
+(define (main-canvas frame table-bitmap card-bitmap)
   (new canvas% 
     [parent frame]
-    [min-height 365]
+    [min-height 500]
     [paint-callback
       (lambda (canvas drawing-context)
         (send drawing-context draw-bitmap table-bitmap 0 0)
-        (send drawing-context draw-bitmap (read-bitmap "resources/test_card.png") 100 100))])
+        (send drawing-context draw-bitmap card-bitmap 100 50)
+        (send drawing-context draw-bitmap card-bitmap 104 54)
+        (send drawing-context draw-bitmap card-bitmap 108 58))])
   )
+
+#|Creates horizontal panel where the "Stand" and "New Card" buttons will be placed
+@param parent-frame|#
+(define (create-horiz-panel parent-frame)
+  (new horizontal-panel% 
+      [parent parent-frame]
+      [alignment (list 'right 'center)]))
+
+#|Creates button to begin game. It disables after being clicked.
+@param horiz-panel : horizontal-panel%
+@param parent-frame : frame%
+@param my-dc : bitmap-dc%
+@param amount-players : int
+|#
+(define (begin-button horiz-panel parent-frame my-dc amount-players)
+(new button% [label "Begin"]
+      [parent horiz-panel]
+      [vert-margin 5]
+      [horiz-margin 5]
+      [callback
+      (lambda (button event)
+        ;(send my-dc clear)
+        (set-turn-mssg (get-current-turn *players*))
+        (send my-dc draw-bitmap (read-bitmap "resources/cards/cardBack_red4.png") *x_croupier* 30)
+        (send my-dc draw-bitmap (read-bitmap "resources/cards/cardClubs10.png") (+ *x_croupier* 20) 30)
+        (send button enable #f)
+        (send parent-frame refresh))]))
+
+#|Creates button for new card. Updates images on canvas. The position of the cards depend on the amount of players.
+@param horiz-panel : horizontal-panel%
+@param parent-frame : frame%
+@param my-dc : bitmap-dc%
+@param amount-players : int
+|#
+(define (new-card-button horiz-panel parent-frame my-dc amount-players)
+(new button% [label "New Card"]
+      [parent horiz-panel]
+      [vert-margin 5]
+      [horiz-margin 5]
+      [callback
+      (lambda (button event)
+        ;(send my-dc clear)
+        ;(send my-dc draw-bitmap (read-bitmap "plainDoge2.jpg") 100 0)
+
+        (send parent-frame refresh))]))
+
+#|Creates button to stand. The player wont receive more cards.
+@param horiz-panel : horizontal-panel%
+@param parent-frame : frame%
+@param my-dc : bitmap-dc%
+|#
+(define (stand-button horiz-panel parent-frame my-dc)
+(new button% [label "Stand"]
+      [parent horiz-panel]
+      [vert-margin 5]
+      [horiz-margin 5]
+      [callback
+      (lambda (button event)
+        ;(send my-dc clear)
+        ;(send my-dc draw-bitmap (read-bitmap "plainDoge2.jpg") 100 0)
+        ;(send button enable #f)
+        (send parent-frame refresh))]))
+
+#|Adds croupier to queue
+@param names : list|#
+(define (add-croupier names)
+  (cons (list "*croupier*" '() #t) names))
 
 #|Starts game by ...|#
 (define (start-game names)
   (send welcome-window show #f)
-  (set-turn-mssg (get-current-turn names))
+  (set! *players* (add-croupier names)) ;sets list with sublists of the names of each player and the croupier
   (let 
-    ([blackjack-table (read-bitmap "resources/table.jpg")])
-    (main-canvas game-window blackjack-table))
+    ([blackjack-table (read-bitmap "resources/table.jpg")]
+    [back-card (read-bitmap "resources/cards/cardBack_red4.png")])
+    (main-canvas game-window blackjack-table back-card)
+    (let 
+      ([horiz-panel (create-horiz-panel game-window)])
+      (let ([drawing-context (new bitmap-dc% [bitmap blackjack-table])])
+        (begin-button horiz-panel game-window drawing-context (length names))
+        (new-card-button horiz-panel game-window drawing-context (length names))
+        (stand-button horiz-panel game-window drawing-context)
+      )))
   (send game-window show #t)
   )
 
